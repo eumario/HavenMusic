4e62d33b69e44e2c324ffc8b36a0ce08a1b765ee
Mario Steele <mario@ruby-im.net>
2025-12-18 10:42:11 -0600

    Created Secrets
    Store API Keys and Secrets inside this file.

---
+++ Library/Secrets.cs
@@ -0,0 +1,7 @@
+namespace HavenMusic.Library;
+
+public class Secrets
+{
+    public static readonly string ClientId = "ENTER_SPOTIFY_CLIENT_ID";
+    public static readonly string ClientSecret = "ENTER_SPOTIFY_CLIENT_SECRET";
+}
