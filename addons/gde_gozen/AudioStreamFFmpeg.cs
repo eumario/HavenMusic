cabc374615d241a8d9e35bbab6ef7ba103731527
Mario Steele <mario@ruby-im.net>
2026-01-13 10:55:11 -0600

    Update GDEGoZen
    Update GDE GoZen.

--- addons/gde_gozen/AudioStreamFFmpeg.cs
+++ addons/gde_gozen/AudioStreamFFmpeg.cs
@@ -1,9 +1,18 @@
 using Godot;
 using System;
+using Godot.Collections;
+using Godot.NativeInterop;
 
 public partial class AudioStreamFFmpeg : AudioStream
 {
     private Variant _backer;
+
+    public bool UseIcy
+    {
+        get => GetUseIcy();
+        set => SetUseIcy(value);
+    }
+    
 
     public AudioStreamFFmpeg()
     {
@@ -18,4 +27,10 @@ public partial class AudioStreamFFmpeg : AudioStream
     public Error Open(string path, int streamIndex = -1) => _backer.AsGodotObject().Call("open", path, streamIndex).As<Error>();
 
     public override AudioStreamPlayback _InstantiatePlayback() => _backer.AsGodotObject().Call("__instantiate_playback").As<AudioStreamPlayback>();
+
+    public override double _GetLength() => _backer.AsGodotObject().Call("get_length").AsDouble();
+    
+    public bool GetUseIcy() => _backer.AsGodotObject().Call("get_use_icy").AsBool();
+    public void SetUseIcy(bool value) => _backer.AsGodotObject().Call("set_use_icy", value);
+
 }
